e29dd6fead2aa6cf938ee8e4b1a5668a00d023aa
Kata <kata@Rustams-MacBook-Pro-2.local>
2021-06-17 12:42:49 +0200

    Introduce test for checking winning along a row

--- TicTacToeTests/GameTests.swift
+++ TicTacToeTests/GameTests.swift
@@ -45,6 +45,16 @@ class GameTests: XCTestCase {
         XCTAssertEqual(game.currentPlayer, Player(mark: "X"))
     }
     
+    func testGameMarkingAllPositionsByOnePlayerAlongARowResultInWinningState() {
+        var game = Game(board: Board(), firstPlayer: Player(mark: "X"), secondPlayer: Player(mark: "O"))
+        game.play(0, 0)
+        game.play(1, 0)
+        game.play(0, 1)
+        game.play(1, 1)
+        game.play(0, 2)
+        XCTAssertEqual(game.state, .winning("X"))
+    }
+    
     private func createBoardWithWithMarkedPosition(size: Int = 3, mark: (row: Int, col: Int, value: String)) -> [[String]] {
         var board = Array<[String]>(repeating: Array<String>(repeating: "", count: size), count: size)
         board[mark.row][mark.col] = mark.value
