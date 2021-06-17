fa4a10c59898f888c7ec594963eceb1d176a5e64
Kata <kata@Rustams-MacBook-Pro-2.local>
2021-06-17 14:00:31 +0200

    Add additional test to verify correct diagonal extraction

---
+++ TicTacToeTests/Array+ExtensionsTests.swift
@@ -0,0 +1,35 @@
+//
+//  Array+ExtensionsTests.swift
+//  TicTacToeTests
+//
+//  Created by Kata on 17/06/2021.
+//
+
+import XCTest
+
+class Array_ExtensionsTests: XCTestCase {
+
+    func testMatrixExtractValuesAlongDiagonal() {
+        var matrix = [
+            ["X", "A", "B"],
+            ["D", "X", "C"],
+            ["E", "F", "X"],
+        ]
+        
+        let diagonal = matrix.principalDiagonal()
+        XCTAssertEqual(diagonal, ["X", "X","X"])
+    }
+    
+    func testMatrixExtractValuesAlongDiagonalReverse() {
+        var matrix = [
+            ["X", "A", "Y"],
+            ["D", "Y", "C"],
+            ["Y", "F", "X"],
+        ]
+        
+        var reversed:[[String]] = matrix.reversed()
+        let diagonal = reversed.principalDiagonal()
+        XCTAssertEqual(diagonal, ["Y", "Y","Y"])
+    }
+
+}
