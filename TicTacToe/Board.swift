bf49046e1a0b11cd9695b9dfa610156c74486f78
Kata <kata@Rustams-MacBook-Pro-2.local>
2021-06-17 10:18:57 +0200

    Implement method for illegal mark verification

--- TicTacToe/Board.swift
+++ TicTacToe/Board.swift
@@ -14,11 +14,18 @@ enum BoardError: Error {
 struct Board {
     var state:[[String]]
     
+    private let size: Int
+    
     init(size: Int) {
+        self.size = size
         state = Array<[String]>(repeating: Array<String>(repeating: "", count: size), count: size)
     }
     
-    mutating func mark(_ row: Int, _ col: Int, mark: String) {
+    mutating func mark(_ row: Int, _ col: Int, mark: String) throws {
+        guard row <= size, col <= size else {
+            throw BoardError.illegalMarkPostion
+        }
+        
         state[row][col] = mark
     }
 }
