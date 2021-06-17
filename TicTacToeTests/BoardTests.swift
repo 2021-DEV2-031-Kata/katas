6833e82c9946b7311c76f75b79aa9330cc6b559f
Kata <kata@Rustams-MacBook-Pro-2.local>
2021-06-17 09:28:02 +0200

    Introduce the concept of a playing board

---
+++ TicTacToeTests/BoardTests.swift
@@ -0,0 +1,17 @@
+//
+//  BoardTests.swift
+//  TicTacToeTests
+//
+//  Created by Kata on 17/06/2021.
+//
+
+import XCTest
+@testable import TicTacToe
+
+class BoardTests: XCTestCase {
+
+    func testBoardInitializesBoardWithCorrectDimensions() {
+        var board = Board()
+    }
+
+}
