92b7c811e2c95a4d3a50b8f7ec9a157f35eaaf51
Kata <kata@Rustams-MacBook-Pro-2.local>
2021-06-17 11:57:58 +0200

    Implement changing player after mark

--- TicTacToe/Game.swift
+++ TicTacToe/Game.swift
@@ -9,20 +9,33 @@ import Foundation
 
 struct Game {
     
-    private(set) var board: Board
     let firstPlayer: Player
     let secondPlayer: Player
+
+    private(set) var board: Board
+    private(set) var currentPlayer: Player
     
-    var currentPlayer: Player {
-        return firstPlayer
+    init(board: Board, firstPlayer: Player, secondPlayer: Player) {
+        self.board = board
+        self.firstPlayer = firstPlayer
+        self.secondPlayer = secondPlayer
+        self.currentPlayer = firstPlayer
     }
     
     mutating func play(_ row: Int, _ col: Int) {
         try? board.mark(row, col, mark: currentPlayer.mark)
+        currentPlayer = changePlayer()
     }
     
     func getBoard() -> [[String]] {
         return board.state
+    }
+    
+    private func changePlayer() -> Player {
+        if currentPlayer.mark == firstPlayer.mark {
+            return secondPlayer
+        }
+        return firstPlayer
     }
     
 }
